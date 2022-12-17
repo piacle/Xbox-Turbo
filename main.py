bc0cdd05fcd136cbd34d483c54e5de688fa9c549
piacle <iwakura@lain.quest>
2022-12-17 12:49:17 -0800

    tui

--- main.py
+++ main.py
@@ -1,87 +1,155 @@
 from threading import Thread
 from json import load, dumps
 from re import match
-from os import system, path
+from os import path
+import warnings
+
 from asyncio import run
-from auth import Auth
-from turbo import Turbo
 from concurrent.futures import ThreadPoolExecutor
-configuration = load(open("configuration.json"))
-system("cls||clear")
-def inpppppppppppp(key, console):
-    console.print(f"[[bold grey93]*[/bold grey93]] {key}: ", end="", highlight=False)
-    keyinp = input()
-    configuration[key] = keyinp
-    with open("configuration.json", 'w') as w:
+from textual.app import App, ComposeResult
+from textual.widgets import Input
+
+
+from api import Auth, Turbo
+from util.alignment import center, center_y
+warnings.simplefilter("ignore", ResourceWarning)
+configuration = load(open("data/configuration.json"))
+placeholder = ""
+limit = 0
+
+
+
+class InputTextbox(App):
+    CSS_PATH = "input.css"
+
+    @staticmethod
+    def valid(value) -> bool:
+        if placeholder == "Gamertag":
+            return not match("[a-zA-Z0-9 ]", value) or value[0] not in ['0', '1', '2', '3', '4', '5', '6', '7', '8',
+                                                                        '9']
+        elif placeholder == "Threads":
+            try:
+                int(value)
+            except ValueError:
+                return False
+            else:
+                return int(value) >= 1
+
+    @staticmethod
+    def compose() -> ComposeResult:
+        yield Input(placeholder=placeholder, id="inp")
+
+    async def on_input_changed(self, message: Input.Changed):
+        if placeholder == "Gamertag" and len(message.value) > limit:
+            self.query_one("#inp", Input).value = message.value[:limit].strip()
+
+    async def on_input_submitted(self):
+        if self.valid(self.query_one("#inp", Input).value.strip()):
+            self.exit(self.query_one('#inp', Input).value.strip())
+        else:
+            self.console.print(
+                "Invalid Value!", justify='right', style='white on #000000')
+
+
+def configure(key, console):
+    console.print(
+        f"[[bold grey93]*[/bold grey93]] {key}: ", end="", highlight=False)
+    btc = input()
+    configuration[key] = btc
+    with open("data/configuration.json", 'w') as w:
         w.write(dumps(configuration, indent=4))
-    return keyinp
+    return btc
+
 
 class Runner:
-    def __init__(self) -> None:
+    def __init__(self):
+        global limit
         self.turbo = Turbo()
-        self.console, self.vaild, self.gamertagSystem, self.auth, self.accounts = self.turbo.console, {"gamertagSystem": ["new", "old"], "auth": ["accounts", "tokens"]}, inpppppppppppp('gamertagSystem', self.console) if not configuration.get('gamertagSystem') else configuration["gamertagSystem"], inpppppppppppp('auth', self.console) if not configuration.get('auth') else configuration["auth"], inpppppppppppp('accounts', self.console) if not configuration.get('accounts') else configuration["accounts"]
-        self.limit = 12 if self.gamertagSystem == "new" else 15
-    async def check(self):
-        errors = []
-        for key in self.vaild:
-            if configuration[key] not in self.vaild[key]:
-                errors.append(f"[[bold red]-[/bold red]] {key} invaild option, vaild ones are: " + ", ".join(self.vaild[key]))
-        if path.isfile(self.accounts) != True:
-            errors.append("[[bold red]-[/bold red]] accounts invaild path")
+        self.console, self.valid, self.gamertagSystem, self.auth, self.accounts = self.turbo.console, {
+            "gamertagSystem": ["new", "old"], "auth": ["accounts", "tokens"]}, configure('gamertagSystem',
+                                                                                         self.console) if not configuration.get(
+            'gamertagSystem') else configuration["gamertagSystem"], configure('auth',
+                                                                              self.console) if not configuration.get(
+            'auth') else configuration["auth"], configure('accounts', self.console) if not configuration.get(
+            'accounts') else configuration["accounts"]
+        limit = 12 if self.gamertagSystem == "new" else 15
+        self.console.clear()
+
+    def check(self):
+        errors = [
+            f"[-] {key} invalid option, valid ones are: " +
+            ", ".join(self.valid[key])
+            for key in self.valid
+            if configuration[key] not in self.valid[key]
+        ]
+        if not path.isfile(self.accounts):
+            errors.append("[[bold red]-[/bold red]] accounts invalid path")
         return errors
 
-    async def start(self):
-        with self.console.status(status="[bold grey85]Checking config[/bold grey85]", spinner='material'):
-            errors = await self.check()
+    def start(self):
+        global placeholder
+        errors = self.check()
         if len(errors) != 0:
-            self.console.print("\n".join(errors) + f"\n[[bold grey93]*[/bold grey93]] Edit configuration.json to fix errors\nPress enter to exit...", highlight=False)
+            self.console.print(center("\n".join(
+                errors) + "\n[*] Edit data/configuration.json to fix errors\nPress enter to exit..."),
+                highlight=False)
             input()
             exit(-1)
-        aU = Auth(self.accounts)
-        a = len(open(self.accounts).read().splitlines())
-        if self.turbo.tag == None:
-            self.console.print(f"[bold grey93]{self.turbo.banner}[/bold grey93]\n[[bold grey85]*[/bold grey85]] Gamertag: ", end="", highlight=None)
-            temp12831 = input().strip()
-            if len(temp12831) > self.limit:
-                system("cls||clear")
-                self.console.print(f"[[bold red]-[/bold red]] Gamertag is greater than {self.limit}",highlight=None)
-                await self.start()
-            else:
-                if not match("[a-zA-Z0-9 ]", temp12831) or temp12831[0] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or temp12831 == "": 
-                    system("cls||clear")
-                    self.console.print(f"[[bold red]-[/bold red]] Invaild Gamertag", highlight=None)
-                    await self.start()
-                else:
-                    self.turbo.tag = temp12831;temp12831=None;del temp12831
-                    self.turbo.rd, self.turbo.cd = {"classicGamertag": self.turbo.tag, "targetGamertagFields": "classicGamertag"} if self.limit == 15 else {"gamertag": self.turbo.tag, "targetGamertagFields": "gamertag"}, {"gamertag": {"classicGamertag": self.turbo.tag}, "preview": False, "useLegacyEntitlement": False} if self.limit == 15 else {"gamertag": {"gamertag": self.turbo.tag, "gamertagSuffix": "", "classicGamertag": self.turbo.tag}, "preview": False, "useLegacyEntitlement": False}
+
+        if self.turbo.tag is None:
+            placeholder = "Gamertag"
+            inputt = InputTextbox()
+            capturedinput100 = inputt.run()
+            if capturedinput100 is None:
+                raise SystemExit
+            self.turbo.tag = capturedinput100
+            del capturedinput100
+            self.turbo.rd, self.turbo.cd = {"classicGamertag": self.turbo.tag,
+                                            "targetGamertagFields": "classicGamertag"} if limit == 15 else {
+                "gamertag": self.turbo.tag, "targetGamertagFields": "gamertag"}, {
+                "gamertag": {"classicGamertag": self.turbo.tag}, "preview": False,
+                "useLegacyEntitlement": False} if limit == 15 else {
+                "gamertag": {"gamertag": self.turbo.tag, "gamertagSuffix": "", "classicGamertag": self.turbo.tag},
+                "preview": False, "useLegacyEntitlement": False}
         
-        if self.turbo.threads == None:
-            self.console.print(f"[[bold grey85]*[/bold grey85]] Threads: ", end="", highlight=None)
-            try:
-                t = int(input());self.turbo.threads=t;t=None;del t
-            except Exception as e:
-                system("cls||clear")
-                self.console.print(f"[[bold red]*[/bold red]] {e}",highlight=None)
-                await self.start()
+        self.console.clear()
+
+        if self.turbo.threads is None:
+            placeholder = "Threads"
+            capturedinput100 = InputTextbox().run()
+            if capturedinput100 is None:
+                raise SystemExit
+            self.turbo.threads = int(capturedinput100)
+            del capturedinput100
         
-        f = ThreadPoolExecutor().submit(run, aU.combolist() if self.auth=="accounts" else aU.jwt())
-        while aU.count != a:
-            self.console.print(f"[[bold grey85]*[/bold grey85]] Loaded accounts: [[bold grey85]{aU.count}[/bold grey85]/[bold grey85]{a}[/bold grey85]]{' '*a}", end="\r", highlight=None)
+        load_accounts = Auth(self.accounts)
+        f = ThreadPoolExecutor().submit(run, load_accounts.combolist() if self.auth == "accounts" else load_accounts.jwt())
+        while load_accounts.count != load_accounts.amount:
+            self.console.print(
+                f"[[bold grey85]*[/bold grey85]] Loaded accounts: [[bold grey85]{load_accounts.count}[/bold grey85]/[bold grey85]{load_accounts.amount}[/bold grey85]]{' ' * load_accounts.amount}", end="\r", highlight=None)
+
         self.turbo.accounts, failed = f.result()
+        self.console.clear()
         if len(self.turbo.accounts) == 0:
-            self.console.print(f"[[bold grey85]*[/bold grey85]] Get{' VAILD' if failed > 0 else ''} {'accounts' if self.auth == 'accounts' else 'tokens'} then use this.{'       '*a}", highlight=None)
+            self.console.print(
+                f"{center_y()}[[bold grey85]*[/bold grey85]] Get{' VALID' if failed > 0 else ''} {'accounts' if self.auth == 'accounts' else 'tokens'} then use this.\nPress enter to exit.", highlight=None, justify='center')
             input()
             exit(-1)
-        self.console.print(f"[[bold grey85]+[/bold grey85]] Loaded {len(self.turbo.accounts)} account(s){'        '*a}\n[[bold grey85]*[/bold grey85]] Failed Loading: {failed} account(s)\n", highlight=False)
-        input("Press enter whenever your ready...")
-        system("cls||clear")
-        self.console.print(f"[bold grey93]{self.turbo.banner}[/bold grey93]")
+        self.console.print(
+            f"{center_y()}[[bold grey85]+[/bold grey85]] Loaded {len(self.turbo.accounts)} account(s)\n[[bold grey85]*[/bold grey85]] Failed Loading: {failed} account(s)\nPress enter to start.\n\n{self.turbo.accounts}", highlight=False, justify='center')
+        self.console.input()
+        self.console.clear()
+        self.console.print(
+            f"{center_y()}[bold grey85]{self.turbo.banner}[/bold grey85]\n", justify='center')
         Thread(target=run, daemon=True, args=[self.turbo.info()]).start()
-        [Thread(target=run, daemon=True, args=[self.turbo.reserve()]).start() for _ in range(self.turbo.threads)]
-        while True: 
+        [Thread(target=run, daemon=True, args=[self.turbo.reserve()]).start()
+         for _ in range(self.turbo.threads)]
+        while True:
             try:
                 pass
             except (KeyboardInterrupt, EOFError):
                 exit(0)
+
+
 if __name__ == '__main__':
-    run(Runner().start())
+     Runner().start()
